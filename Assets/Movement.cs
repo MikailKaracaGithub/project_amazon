57cc830a57e740cb0a75633281dc6c69e1b29785
Xavaltir <tombis1999@hotmail.com>
2019-10-25 20:44:45 +0200

    Initial Commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+}
